cbc050c78d4b8cbfd5ea3498bb18300e2d0f00c9
nh99 <neil.harwani@mantratec.com>
2018-01-30 10:44:41 +0530

    Initial commit

---
+++ app/src/main/java/com/mantratec/mapsdemo/MainInterface.java
@@ -0,0 +1,16 @@
+package com.mantratec.mapsdemo;
+
+/**
+ * Created by admin on 12/1/2017.
+ */
+
+public interface MainInterface
+{
+    public void setupViews();
+
+    public void setupToolbar();
+
+    public void setupData();
+
+    public void setupClickEvents();
+}
